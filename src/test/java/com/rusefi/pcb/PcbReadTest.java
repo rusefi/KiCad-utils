d270416c6af98b8c5b899492b2732b3856f6a6ba
rusefillc <sdfsdfqsf2334234234>
2021-01-07 04:50:01 -0500

    weird

--- src/test/java/com/rusefi/pcb/PcbReadTest.java
+++ src/test/java/com/rusefi/pcb/PcbReadTest.java
@@ -20,23 +20,12 @@ public class PcbReadTest {
     public void testParseKicad4() {
         String string = "(kicad_pcb " +
                 "(version 4) (host pcbnew \"(2014-07-21 BZR 5016)-product\")\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "  (net 0 \"\")\n" +
-                "  (net 205 \"GNDA\")\n" +
-                "\n" +
-                "  \n" +
                 "  (net_class Default \"This is the default net class.\"\n" +
                 "\t(add_net Net1)\n" +
                 "  )\n" +
                 "  \n" +
                 "(gr_line (start 17.24988 -105.76185) (end 15.64980 -105.76185) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
                 "(gr_line (start 15.64980 -105.76185) (end 15.64980 -104.93783) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
-                "(gr_line (start 15.64980 -104.93783) (end 17.24988 -104.93783) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
-                "(gr_line (start 17.24988 -104.93783) (end 17.24988 -105.76185) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
-                "(gr_line (start 32.50003 3.80002) (end 111.90002 3.80002) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
                 "(gr_line (start 111.90002 3.80002) (end 111.90002 -30.05001) (angle 90) (layer F.CrtYd) (width 0.2))\n" +
                 ")";
         PcbNode node = PcbNode.parse(string);
